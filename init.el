3ee898f3aba25e4077c3b2cd265f1d343d3bd34e
DaMoo <git@damoo.zone>
2018-06-05 10:59:04 -0400

    Add magit

--- init.el
+++ init.el
@@ -57,6 +57,18 @@
 (use-package try
   :ensure t)
 
+(use-package magit
+  :config
+  (setq magit-completing-read-function 'ivy-completing-read)
+  :bind
+  ("C-x g s" . magit-status)
+  ("C-x g x" . magit-checkout)
+  ("C-x g c" . magit-commit)
+  ("C-x g p" . magit-push)
+  ("C-x g u" . magit-pull)
+  ("C-x g e" . magit-ediff-resolve)
+  ("C-x g r" . magit-rebase-interactive))
+
 (use-package which-key
   :ensure t
   :config (which-key-mode))
