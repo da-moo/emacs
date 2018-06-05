e62abe67e6e089f1c06aeabf24d577dd0d7f9696
DaMoo <git@damoo.zone>
2018-06-05 11:01:55 -0400

    Set use-package to always ensure

--- init.el
+++ init.el
@@ -52,10 +52,10 @@
 (unless (package-installed-p 'use-package)
 	(package-refresh-contents)
 	(package-install 'use-package))
+(setq use-package-always-ensure t)
 
 ;; Custom packages
-(use-package try
-  :ensure t)
+(use-package try)
 
 (use-package magit
   :config
@@ -70,11 +70,9 @@
   ("C-x g r" . magit-rebase-interactive))
 
 (use-package which-key
-  :ensure t
   :config (which-key-mode))
 
 (use-package ace-window
-  :ensure t
   :defer 1
   :config
   (global-set-key [remap other-window] 'ace-window)
@@ -84,23 +82,18 @@
   nil :foreground "red" :weight 'bold :height 3.0))
 
 (use-package avy
-  :ensure t
   :bind ("C-." . avy-goto-char)
   :config
   (setq avy-keys '(?a ?o ?e ?u ?i ?d ?h ?t ?n ?s)))
 
-(use-package solarized-theme
-  :ensure t)
+(use-package solarized-theme)
 
-(use-package oceanic-theme
-  :ensure t)
+(use-package oceanic-theme)
 
 (use-package zenburn-theme
-  :ensure t
   :config (load-theme 'zenburn t))
 
 (use-package counsel
-  :ensure t
   :bind (("\C-s" . swiper)
 	 ("C-c C-r" . ivy-resume)
 	 ("<f6>" . ivy-resume)
@@ -123,11 +116,9 @@
   'counsel-minibuffer-history))
 
 (use-package company
-  :ensure t
   :config (global-company-mode) (setq company-idle-delay t))
 
 (use-package org
-  :ensure t
   :bind (("\C-c l" . org-store-link)
 	 ("\C-c a" . org-agenda)
 	 ("\C-c c" . org-capture)
@@ -136,12 +127,10 @@
   (setq org-log-done t))
 
 (use-package nov
-  :ensure t
   :config (add-to-list 'auto-mode-alist '("\\.epub\\'"
 					  . nov-mode)))
 
-(use-package markdown-mode
-  :ensure t)
+(use-package markdown-mode)
 
 ;;;;;;;;;;;;;;;;;
 ;; Auto-config ;;
