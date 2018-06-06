417a4ce077431ff1f880ae88372526c4018e98ad
DaMoo <git@damoo.zone>
2018-06-06 14:58:48 -0400

    Remove auto-config section

--- init.el
+++ init.el
@@ -196,17 +196,6 @@
   :after flycheck
   :hook (flycheck-mode . flycheck-posframe-mode)
   :config
-;;;;;;;;;;;;;;;;;
-;; Auto-config ;;
-;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (markdown-mode nov zenburn-theme which-key use-package try solarized-theme org oceanic-theme counsel company ace-window))))
   (flycheck-posframe-configure-pretty-defaults)
   (set-face-background 'flycheck-posframe-background-face
 		       (face-attribute 'secondary-selection :background)))
